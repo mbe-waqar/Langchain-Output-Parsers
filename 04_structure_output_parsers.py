7780980094c6cf3fcd7ae4707c77e9e69ca93d3a
Waqar Ali <waqar0343930@gmail.com>
2025-08-08 17:15:21 +0500

    structure output parsers

--- 04_structure_output_parsers.py
+++ 04_structure_output_parsers.py
@@ -12,3 +12,16 @@ schema = [
     ResponseSchema(name='fact2', description='fact 2 about the topic'),
     ResponseSchema(name='fact3', description='fact 3 about the topic'),
 ]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give me three facts about {topic}. /n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+result = chain.invoke({'topic':'Black Hole'})
+
+print(result)
